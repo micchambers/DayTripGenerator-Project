7ebd4ec745d9274a17858e32013142cb760bb775
micchambers <micolc76@gmail.com>
2022-11-07 20:39:16 -0500

    Lists of destinations, restaurants, entertainment and transportation written

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print('Hello World!')
+destination_list = ['ATL', "LA", "NYC",]
+transportation_list = ["driving", "taking the train", "biking"]
+entertainment_list = ["watching Hunter x Hunter (peak)", "going to the opera", "watching a Kung Fu movie"]
+restaurant_list = ['BGR Grille', "Willy's Mexicana Grill", "Slim & Husky's Pizza Beeria"]
