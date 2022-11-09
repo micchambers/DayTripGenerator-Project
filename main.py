5a86a257c1ed88218b32d74fe8449a1785db3d0c
micchambers <micolc76@gmail.com>
2022-11-09 12:12:15 -0500

    testing new repo setup

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ destination_list = ['ATL', "LA", "NYC",]
 transportation_list = ["driving", "taking the train", "biking"]
 entertainment_list = ["watching Hunter x Hunter (peak)", "going to the opera", "watching a Kung Fu movie"]
 restaurant_list = ['BGR Grille', "Willy's Mexicana Grill", "Slim & Husky's Pizza Beeria"]
+
+def day_trip_generator():
+    print("On today's trip, you'll be going to {} by {}, stopping to {} and to eat at {}")
