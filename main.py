58f0a111b12fb039af6ef3b005629c14c2ffa937
micchambers <micolc76@gmail.com>
2022-11-10 01:50:02 -0500

    Created a functions file and defined randomization functions

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+
 destination_list = ['ATL', "LA", "NYC",]
 transportation_list = ["driving", "taking the train", "biking"]
-entertainment_list = ["watching Hunter x Hunter (peak)", "going to the opera", "watching a Kung Fu movie"]
+entertainment_list = ["watch Hunter x Hunter (peak)", "go to the opera", "watch a Kung Fu movie"]
 restaurant_list = ['BGR Grille', "Willy's Mexicana Grill", "Slim & Husky's Pizza Beeria"]
 
-def day_trip_generator():
-    print("On today's trip, you'll be going to {} by {}, stopping to {} and to eat at {}")
+
+
+
+# def day_trip_generator():
+#     print("On today's trip, you'll be going to {} by {}, stopping to {} and to eat at {}")
+
+
+
