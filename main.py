62e99e1471132d7d7f07b46c79988fca77eedca5
micchambers <micolc76@gmail.com>
2022-11-14 13:24:59 -0500

    Moved contents of function.py to main.py for the sake of simplicity. Completed basic trip generator function.

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
+import random
+print('')
+print('')
+
 
 destination_list = ['ATL', "LA", "NYC",]
 transportation_list = ["driving", "taking the train", "biking"]
-entertainment_list = ["watch Hunter x Hunter (peak)", "go to the opera", "watch a Kung Fu movie"]
+entertainment_list = ["read Hunter x Hunter (peak)", "go to the opera", "watch a Kung Fu movie"]
 restaurant_list = ['BGR Grille', "Willy's Mexicana Grill", "Slim & Husky's Pizza Beeria"]
 
 
+def random_item_picker(list):
+    random_item = random.choice(list)
+    return random_item
+
+def run_trip_generator(list_1, list_2, list_3, list_4):
+    chosen_dest = random_item_picker(list_1)
+    chosen_trans = random_item_picker(list_2)
+    chosen_ent = random_item_picker(list_2)
+    chosen_rest = random_item_picker(list_3)
+    print(f"On today's trip, you'll be going to {chosen_dest} by {chosen_trans}, stopping to {chosen_ent} and to eat at {chosen_rest}")
+    
 
 
-# def day_trip_generator():
-#     print("On today's trip, you'll be going to {} by {}, stopping to {} and to eat at {}")
+run_trip_generator(destination_list, transportation_list, entertainment_list, restaurant_list)
 
 
 
+print('')
+print('')
